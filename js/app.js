6848644a2aba01d1a6931f1b86cfbc8bb976ef53
Norman Trujillo <natrujillo9@gmail.com>
2024-02-27 21:28:50 -0500

    Add js for slider

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
-$(document).ready(function(){
-  $(".navbar-toggle").click(function(){
+$(document).ready(function () {
+  $(".navbar-toggle").click(function () {
     $(this).toggleClass("active");
+  });
+});
+
+$(document).ready(function () {
+  $("#myCarousel").carousel({
+    interval: 3000, // Change the interval in milliseconds (default is 5000)
   });
 });
